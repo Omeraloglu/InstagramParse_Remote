150acb7d38361f4bee7d95e133be3e541d18f85b
Omer <omeraloglu16@hotmail.com>
2022-06-23 01:05:42 +0300

    adjust constraints

--- InstagramParse/uploadVC.swift
+++ InstagramParse/uploadVC.swift
@@ -7,12 +7,48 @@
 
 import UIKit
 
-class uploadVC: UIViewController {
+class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var postButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let keyboardRecognizere = UITapGestureRecognizer(target: self, action: #selector(uploadVC.hideKeyboard))
+        self.view.addGestureRecognizer(keyboardRecognizere)
         // Do any additional setup after loading the view.
+        
+        
+        postImage.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(uploadVC.choosePhoto))
+        postImage.addGestureRecognizer(gestureRecognizer)
+        
+        postButton.isEnabled = false
     }
 
-
+    @IBAction func postClicked(_ sender: Any) {
+        postButton.isEnabled = false
+    }
+    
+    @objc func hideKeyboard(){
+        self.view.endEditing(true)
+    }
+    @objc func choosePhoto() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        pickerController.allowsEditing = true
+        present(pickerController, animated: true, completion: nil)
+    
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        postImage.image = info[.originalImage] as? UIImage
+        self.dismiss(animated:true, completion: nil)
+        postButton.isEnabled = true
+        
+    }
+    
 }
