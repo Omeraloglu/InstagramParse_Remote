6f6281ed8afb34134a1f931ebfc850a9b7ec626d
Omer <omeraloglu16@hotmail.com>
2022-06-23 17:40:26 +0300

    feed okey

--- InstagramParse/feedVC.swift
+++ InstagramParse/feedVC.swift
@@ -8,14 +8,72 @@
 import UIKit
 import Parse
 
-class feedVC: UIViewController {
+class feedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-   
+    var postOwnerArray = [String]()
+    var postCommentArray = [String]()
+    var postUUIDArray = [String]()
+    var postImageArray = [PFFileObject]()
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
+    }
+    
+    func getData(){
+        let query = PFQuery(className: "Post")
+        query.findObjectsInBackground { objects, error in
+            if error != nil{
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                if objects!.count > 0 {
+                    for object in objects!{
+                        self.postOwnerArray.append(object.object(forKey: "postowner") as! String)
+                        self.postCommentArray.append(object.object(forKey: "postcomment") as! String)
+                        self.postUUIDArray.append(object.object(forKey: "postuuid") as! String)
+                        self.postImageArray.append(object.object(forKey: "postimage") as! PFFileObject)
+                    }
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postOwnerArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! feedCell
+        
+        cell.userNameLabel.text = postOwnerArray[indexPath.row]
+        cell.postCommentTExt.text = postCommentArray[indexPath.row]
+        cell.postUUIDLabel.text = postUUIDArray[indexPath.row]
+        
+        postImageArray[indexPath.row].getDataInBackground { data, error in
+            if error != nil{
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }else {
+                cell.postImage.image = UIImage(data: data!)
+                
+            }
+        }
+        
+        return cell
+        
     }
 
     @IBAction func logOutClicked(_ sender: Any) {
